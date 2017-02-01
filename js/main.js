24e7651eac65bb76e19faae6517ec683e32550df
Student <student@ECAW01152-PC10.local>
2017-01-31 19:29:38 -0500

    note-04-conditional

---
+++ js/main.js
@@ -0,0 +1,26 @@
+ var num = 58
+
+ if (num == 45){
+ 	document.write('<br> the number is 45');
+ }	else {
+ 	document.write('<br> the number is not 45');
+
+ }
+
+ var jenny = 19;
+ var henry = 20;
+
+ var username = true;
+ var password = true;
+
+   if (username && password === true){
+   	document.write('<br>access grantent');
+   } else if (username === true && password == false) {
+   	    document.write('<br>your password is worng');
+        } else if (username === false && password == true) {
+        	document.write('<br>your password is worng');
+
+        } else{
+        	document.write('<br>access denied');
+        }
+
